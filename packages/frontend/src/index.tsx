90ee665d0e586cf7708ea5f849097b5c8cafba9c
tonya <tonya@wolfram.com>
2024-09-22 00:18:58 +0100

    Add debugging back ingto front end

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,50 +1,45 @@
-// import { createRoot } from 'react-dom/client';
-// import React from 'react';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
 
-// import { io } from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 import Frontend from './App';
 import { useWL, WLProvider } from './hooks/useWL';
 export { Frontend, useWL, WLProvider };
 
-// import WebHandler from '@wrb/web/src/renderer/WebHandler';
+import WebHandler from '@wrb/web/src/renderer/WebHandler';
 
-// const container = document.getElementById('root') as HTMLElement;
-// 	const root = createRoot(container);
-// 	root.render(
-// 		<React.StrictMode>
-// 			<Frontend api={webHandler.api} />
-// 		</React.StrictMode>,
-// 	);
+/*
+ * This is used to run the frontend in development mode and should not run in production.
+ */
+if (process.env.NODE_ENV === 'development') {
+	const port = process.env.PORT || 3000;
+	const socket = io(`localhost:${port}`);
+	const webHandler = new WebHandler(socket);
 
-// if (process.env.NODE_ENV === 'development') {
-// 	console.log('Development mode');
-// 	const socket = io('localhost:3000');
-// 	const webHandler = new WebHandler(socket);
+	const container = document.getElementById('root') as HTMLElement;
+	const root = createRoot(container);
+	root.render(
+		<React.StrictMode>
+			<Frontend api={webHandler.api} />
+		</React.StrictMode>,
+	);
 
-// 	const container = document.getElementById('root') as HTMLElement;
-// 	const root = createRoot(container);
-// 	root.render(
-// 		<React.StrictMode>
-// 			<Frontend api={webHandler.api} />
-// 		</React.StrictMode>,
-// 	);
+	webHandler.api.ipc.on('connect', () => {
+		console.log('Connected to server');
+		webHandler.api.ipc.send('start-wl');
+	});
 
-// 	webHandler.api.ipc.on('connect', () => {
-// 		console.log('Connected to server');
-// 		webHandler.api.ipc.send('start-wl');
-// 	});
+	webHandler.api.ipc.on('disconnect', () => {
+		console.log('Disconnected from server');
+	});
 
-// 	webHandler.api.ipc.on('disconnect', () => {
-// 		console.log('Disconnected from server');
-// 	});
+	window.addEventListener('beforeunload', () => {
+		socket.disconnect();
+	});
 
-// 	window.addEventListener('beforeunload', () => {
-// 		socket.disconnect();
-// 	});
-
-// 	// Debug
-// 	webHandler.api.ipc.on('wl-status', (code) => {
-// 		console.log('Wolfram Language status:', code);
-// 	});
-// }
+	// Debug
+	webHandler.api.ipc.on('wl-status', (code) => {
+		console.log('Wolfram Language status:', code);
+	});
+}
