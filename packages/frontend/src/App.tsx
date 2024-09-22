ef14cfd14e384ef5e94fea2ac9338e0d251672e6
tonya <tonya@wolfram.com>
2024-09-22 02:26:45 +0100

    Move document title to frontend
    So the developer doesnt have to define the document title separately for each deployement, it is now set using `document.title = ...` from within the FE

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import { Handler } from './api';
 // Packages
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useEffect } from 'react';
 
 // Hooks
 import { WLProvider, useWL } from './hooks/useWL';
@@ -14,6 +14,8 @@ import './css/App.css';
 // Components
 import Demo from './components/pages/Demo';
 import LoadingScreen from './components/pages/LoadingScreen';
+
+import { Handler } from './api';
 
 const theme = createTheme({
 	palette: {
@@ -38,6 +40,9 @@ function AppContent() {
 }
 
 export default function Frontend({ api }: { api: Handler }) {
+	useEffect(() => {
+		document.title = 'WRB: Example Project';
+	});
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
