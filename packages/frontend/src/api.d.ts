cd4d7c35e84ec724fb262acfb83d8fd785b33e8f
tonya <tonya@wolfram.com>
2024-10-17 21:06:20 +0100

    mostly minor type changes

--- packages/frontend/src/api.d.ts
+++ packages/frontend/src/api.d.ts
@@ -1,16 +1,11 @@
-/* These are placeholder definitions for the electron & webhook api.
- * They are left as dormant placeholder types until they are defined in their
- * respective packages.
- * TODO: There has to be a better way to do this...
- */
-
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
-type Channels = string;
+export type Channels = string;
+export type Environments = 'web' | 'electron' | 'jest';
 
 export interface Handler {
-	env: string;
+	env: Environments;
 	ipc: {
 		send(channel: Channels, ...args: unknown[]);
 		on(channel: Channels, func: (...args: unknown[]) => void);
